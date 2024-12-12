f0d2c0fa0bfb8277a7b28d7ebca003a3d0ca80ae
Sanikhsan12 <lordkazumasan12@gmail.com>
2024-12-12 13:17:59 +0700

    logic proses

--- resources/views/livewire/login-component.blade.php
+++ resources/views/livewire/login-component.blade.php
@@ -41,6 +41,7 @@
                 <button
                     type="submit"
                     class="w-full bg-blue-500 text-white py-2 px-4 rounded-full hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                    wire.click="proses"
                 >
                     Login
                 </button>
