2fd2fa7932a799db65a9392734287614ce19bc7c
Sanikhsan12 <lordkazumasan12@gmail.com>
2024-12-27 17:39:49 +0700

    change middleware

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
-use illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Auth;
 
 class RoleMiddleware
 {
