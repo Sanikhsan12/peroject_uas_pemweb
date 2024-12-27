e0b1fd3b3e4eeda0b5241a933f3f804ffc8a6f29
Sanikhsan12 <lordkazumasan12@gmail.com>
2024-12-27 16:50:47 +0700

    update welecome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,13 +7,13 @@
     <!-- link CDn Tailwind -->
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="bg-sky-500 flex items-center justify-center h-screen ">
-    <div class="p-6 bg-blue-800 rounded-lg shadow-lg w-full sm:w-96">
-    <h1 class="text-center text-3xl font-semibold text-neutral-50 Text-center py-4">Selamat Datang Di Aplikasi Peminjaman Buku</h1>
-        <p class="text-center text-neutral-50 mb-6 Text-center">Kami menyediakan berbagai buku yang dapat Anda pinjam. Mulai dengan login untuk melanjutkan.</p>
+<body class="bg-[url('{{ asset('asset/welecome_page.jpg') }}')] flex items-center justify-center h-screen bg-no-repeat bg-cover">
+    <div class="text-center flex flex-col items-center">
+        <h1 class="text-center text-6xl font-semibold text-white py-4">Selamat Datang Di Aplikasi Peminjaman Buku</h1>
+        <p class="text-center text-lg font-semibold text-neutral-50 mb-6 px-3 py-3">Kami menyediakan berbagai buku yang dapat Anda pinjam. Mulai dengan login untuk melanjutkan.</p>
         <div class="flex justify-center">
             <a href="/login">
-                <button class=" item-justify px-6 py-3 bg-blue-500 text-white rounded-full text-lg hover:bg-blue-600 transition duration-300">
+                <button class="px-6 py-3 bg-orange-950 text-white rounded-full text-lg hover:bg-orange-700 transition duration-300">
                     Get Started
                 </button>
             </a>
